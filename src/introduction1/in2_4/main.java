6ff56d4773b03fcf1de1402ecb0b68bbca174257
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-23 22:46:30 +0900

    2_4

---
+++ src/introduction1/in2_4/main.java
@@ -0,0 +1,10 @@
+package introduction1.in2_4;
+
+public class main {
+    public static void main(String[] args) {
+        char a = "a";
+        char b ='abc';
+        char c = 89;
+        char d = null;
+    }
+}
