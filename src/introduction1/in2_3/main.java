5729e9a60da63fe001f07f9d14c052c052dc0543
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-23 22:37:08 +0900

    2_3

---
+++ src/introduction1/in2_3/main.java
@@ -0,0 +1,14 @@
+package introduction1.in2_3;
+
+public class main {
+    public static void main(String[] args) {
+        // 8進数
+        int a = 0877;
+        
+        // 16進数
+        int b = 0x10B;
+
+        // 2進数
+        int c = 0b100001011;
+    }
+}
