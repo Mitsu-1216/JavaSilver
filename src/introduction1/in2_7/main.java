630b7e999f7756d239bc4c0fab0f4533f8b4090c
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-24 08:08:56 +0900

    2_8A

--- src/introduction1/in2_7/main.java
+++ src/introduction1/in2_7/main.java
@@ -2,7 +2,7 @@ package introduction1.in2_7;
 
 public class main {
     public static void main(String[] args) {
-        Sample s = new Sample("sample");
+        A s = new A("sample");
         s.test();
     }
 }
