b2227408e9e91477ce275f1ad6b8c694d9eee143
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-25 07:52:10 +0900

    2_11

---
+++ src/introduction1/in2_11/main.java
@@ -0,0 +1,8 @@
+package introduction1.in2_11;
+
+public class main {
+    public static void main(String[] args) {
+        String str = "abcde";
+        System.out.println(str.charAt(5));
+    }
+}
