254eeb1a31184ebea57ad28a5f59342c1975755f
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-26 08:06:39 +0900

    2_16

---
+++ src/introduction1/in2_16/main.java
@@ -0,0 +1,8 @@
+package introduction1.in2_16;
+
+public class main {
+    public static void main(String[] args) {
+        String str = "abcde";
+        System.out.println(str.substring(1,3).startsWith("b"));
+    }
+}
