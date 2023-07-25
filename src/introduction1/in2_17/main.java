803510318ac23908214a73a7dad7adfe57320e50
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-26 08:12:03 +0900

    2_17

---
+++ src/introduction1/in2_17/main.java
@@ -0,0 +1,8 @@
+package introduction1.in2_17;
+
+public class main {
+    public static void main(String[] args) {
+        String str = "Hello,".concat("Java!");
+        System.out.println(str);
+    }
+}
