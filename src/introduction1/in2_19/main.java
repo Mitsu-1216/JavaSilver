41a0cb1ab8b35b6c050ba31f0911fac233cc52fc
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-27 07:46:00 +0900

    2_19

---
+++ src/introduction1/in2_19/main.java
@@ -0,0 +1,9 @@
+package introduction1.in2_19;
+
+public class main {
+    public static void main(String[] args) {
+        String str = "null";
+        str += "null";
+        System.out.println(str);
+    }
+}
