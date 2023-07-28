41e7b4bcba546e2a7dd55f8d0b3ec5881ae139af
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-29 07:45:06 +0900

    3_1

---
+++ src/introduction1/in3_1/main.java
@@ -0,0 +1,9 @@
+package introduction1.in3_1;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = a += 5;
+        System.out.println(a + b);
+    }
+}
