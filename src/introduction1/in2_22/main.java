4407cbf93dba7bfe901166bc671060e4e68b3f9e
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-29 07:39:20 +0900

    2_22

---
+++ src/introduction1/in2_22/main.java
@@ -0,0 +1,8 @@
+package introduction1.in2_22;
+
+public class main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("abcde");
+        System.out.println(sb.indexOf("bcd"));
+    }
+}
