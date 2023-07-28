e2f3b038868c4d8b37ac400eba599b35d0476a07
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-29 07:36:40 +0900

    2_21

---
+++ src/introduction1/in2_21/main.java
@@ -0,0 +1,11 @@
+package introduction1.in2_21;
+
+public class main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("abcde");
+        sb.reverse();
+        sb.replace(1,3,"a");
+        System.out.println(sb);
+    }
+}
