88664032aba7c46298bf6a22fe4099c88dab9066
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-30 11:08:34 +0900

    3_5

---
+++ src/introduction1/in3_5/main.java
@@ -0,0 +1,9 @@
+package introduction1.in3_5;
+
+public class main {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = true;
+        System.out.println(a <= b);
+    }
+}
