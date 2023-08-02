dc039723a26b46079eec9b2bc0a8eb206be0c082
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-03 08:35:34 +0900

    3_11

---
+++ src/introduction1/in3_11/main.java
@@ -0,0 +1,9 @@
+package introduction1.in3_11;
+
+public class main {
+    public static void main(String[] args) {
+        Object a = new Object();
+        Object b = null;
+        System.out.println(a.equals(b));
+    }
+}
