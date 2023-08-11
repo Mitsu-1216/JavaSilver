f84d33a1dbf31cbd0439320e287a8a08e315801e
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-11 14:23:12 +0900

    3_17

--- src/introduction1/in3_17/main.java
+++ src/introduction1/in3_17/main.java
@@ -1,10 +1,14 @@
 package introduction1.in3_17;
 
 public class main {
-    public static void main(String[] args) { 
-        if(false)
-        System.out.print("A");
-        System.out.print("B");
+    public static void main(String[] args) {
+        int num = 10;
+        if (num < 10)
+            System.out.print("A");
+        else
+            System.out.print("B");
+        if (num == 10)
+            System.out.print("C");
 
     }
 }
