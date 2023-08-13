79fa2a14205135124f2e396dad7a27b8cbb3d4ad
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-13 19:39:56 +0900

    3_18

---
+++ src/introduction1/in3_18/main.java
@@ -0,0 +1,17 @@
+package introduction1.in3_18;
+
+public class main {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num == 100)
+            System.out.print("A");
+        else if (10 < num)
+            System.out.print("B");
+        else if (num == 10)
+            System.out.print("C");
+        else
+        if (num == 10)
+            System.out.print("D");
+
+    }
+}
