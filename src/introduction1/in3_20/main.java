dfa0f349b7ad0e10b5f631d14e873018ba85aef6
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-15 20:45:27 +0900

    3_20

---
+++ src/introduction1/in3_20/main.java
@@ -0,0 +1,23 @@
+package introduction1.in3_20;
+
+public class main {
+    public static void main(String[] args) {
+        final int NUM = 10;
+        int num = 10;
+        switch (num) {
+            case "10":
+            System.out.print("A");
+            break;
+            case num:
+                System.out.print("B");
+                break;
+            case 5*5:
+                System.out.print("C");
+                break;
+            case NUM:
+                System.out.print("D");
+                break;
+        }
+
+    }
+}
