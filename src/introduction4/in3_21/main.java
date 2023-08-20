958a854236a3266d876890c8667460b178285bb9
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-21 08:28:54 +0900

    in4_2

---
+++ src/introduction4/in3_21/main.java
@@ -0,0 +1,17 @@
+package introduction1.in3_21;
+
+public class main {
+    public static void main(String[] args) {
+        int num = 1;
+        switch (num) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.print("A");
+            case 4:
+                System.out.print("B");
+            default:
+                System.out.print("C");
+        }
+    }
+}
---
+++ src/introduction4/in4_2/main.java
@@ -0,0 +1,10 @@
+package introduction4.in4_2;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.print(a++);
+        } while (a < 5);
+    }
+}
