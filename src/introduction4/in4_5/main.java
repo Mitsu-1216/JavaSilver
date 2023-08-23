20dd8d69960ef43c9c8e4c082e859c0fb4560dcd
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-24 08:33:49 +0900

    4_5

---
+++ src/introduction4/in4_5/main.java
@@ -0,0 +1,13 @@
+package introduction4.in4_5;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 1;
+        for (int b = 2 , total = 0; b <= 5; b++) {
+        total = a * b;
+            System.out.print(total);
+    
+    }
+        // System.out.print(total);
+    }
+}
