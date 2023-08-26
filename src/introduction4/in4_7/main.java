14c7ed0dbd14e5f92b3ecf862aad13ac5495b82e
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-26 16:45:33 +0900

    4_7

---
+++ src/introduction4/in4_7/main.java
@@ -0,0 +1,10 @@
+package introduction4.in4_7;
+
+public class main {
+    public static void main(String[] args) {
+        for (int i = 0, j = 0; i < 3,j < 5; i++) {
+            System.out.print(i++);
+            j += i;
+        }
+    }
+}
