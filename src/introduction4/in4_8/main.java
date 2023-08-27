29a2ba92f60f55573ec4382073ad7eea71d98ed1
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-28 08:20:16 +0900

    4_8

---
+++ src/introduction4/in4_8/main.java
@@ -0,0 +1,13 @@
+package introduction4.in4_8;
+
+public class main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++, period()) {
+            System.out.print(i);
+        }
+    }
+
+    public static void period() {
+        System.out.print(",");
+    }
+}
