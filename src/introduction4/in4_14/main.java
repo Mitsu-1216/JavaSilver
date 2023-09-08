084134736b0d89d68aaec9541b4f3de6e56ea914
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-09-08 20:58:02 +0900

    4_15

---
+++ src/introduction4/in4_14/main.java
@@ -0,0 +1,12 @@
+package introduction4.in4_14;
+
+public class main {
+    public static void main(String[] args) {
+
+        int num = 10;
+        do {
+            num++;
+        } while (++num < 10);
+        System.out.println(num);
+    }
+}
