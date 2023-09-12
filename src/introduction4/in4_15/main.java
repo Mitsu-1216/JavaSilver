9979e5351eec73b360668c1c28904e4ada7c7053
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-09-13 08:13:11 +0900

    4_15

--- src/introduction4/in4_15/main.java
+++ src/introduction4/in4_15/main.java
@@ -2,8 +2,11 @@ package introduction4.in4_15;
 
 public class main {
     public static void main(String[] args) {
-
-        int num = 10;
+        int[] array={1,2,3,4,5};
+        int total = 0;
+        for (){
+            
+        }
         do {
             num++;
         } while (++num < 10);
