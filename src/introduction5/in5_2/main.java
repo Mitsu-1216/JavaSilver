8439daf64cc1308102cd6e09779fd98d3054cb62
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-09-17 18:14:55 +0900

    introduction5

---
+++ src/introduction5/in5_2/main.java
@@ -0,0 +1,18 @@
+package introduction5.in5_2;
+
+public class main {
+    public static void main(String[] args) {
+        int total = 0;
+        a: for (int i = 0; i < 5; i++) {
+            b: for (int j = 0; j < 5; j++) {
+                if (i % 2 == 0)
+                    continue a;
+                if (3 < j)
+                    break b;
+                total += j;
+            }
+
+        }
+        System.out.println(total);
+    }
+}
