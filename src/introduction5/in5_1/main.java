6ce71c4a0090db30ba1ed7b34339f94b541ffeb7
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-09-20 07:11:24 +0900

    5_1

--- src/introduction5/in5_1/main.java
+++ src/introduction5/in5_1/main.java
@@ -2,13 +2,7 @@ package introduction5.in5_1;
 
 public class main {
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 4, 5 };
-        int total = 0;
-        for (int i : array) {
-            if (i % 2 == 0)
-                continue;
-            total += i;
-        }
-        System.out.println(total);
+        int[] array = new int[0];
+        System.out.println(array);
     }
 }
